7eae39b166b4b51e9707fcfe39e70a27b0f8becf
Sagar <sagar.muttappanavar@gmail.com>
2024-02-22 00:50:30 +0530

    Changes in wait statements

--- src/main/java/test/SeleniumFrameWork/actions/ActionHelper.java
+++ src/main/java/test/SeleniumFrameWork/actions/ActionHelper.java
@@ -1,12 +1,15 @@
 package test.SeleniumFrameWork.actions;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import test.SeleniumFrameWork.base.BaseTest;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ActionHelper {
 	WebDriver driver;
+	
 	public ActionHelper(WebDriver driver) {
 		this.driver = driver;
 	}
@@ -18,5 +21,10 @@ public class ActionHelper {
 	public static void sendKeys(WebElement element, String data) {
 		element.clear();
 		element.sendKeys(data);
+	}
+	
+	public void waitUntilElementVisible(WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(element));
 	}
 }
